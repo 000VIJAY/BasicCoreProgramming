49afb9e4ffb52a9bfc7924941abe428244ae3b18
000VIJAY <vijay893689@gmail.com>
2022-06-17 11:54:37 +0530

    Added Number is even or odd

--- Program.cs
+++ Program.cs
@@ -1,36 +1,10 @@
-﻿Console.WriteLine("Welcome to flip coin");
-Console.WriteLine("Write Number of time wants to flip coin ");
-int head = 0;
-int numOfTimeCoinIstail = 0;
-int numOfTimeCoinIsHead = 0;
+﻿Console.WriteLine("write a number");
 int num = Convert.ToInt32(Console.ReadLine());
-int[] coin = new int[num];
-for (int i = 0; i < coin.Length; i++)
+if (num % 2 == 0)
 {
-    Random rnd = new Random();
-    int flipCoin = rnd.Next(2);
-    Console.WriteLine("flipCoin : " + flipCoin);
-    
-    if (flipCoin <= head)
-    {
-        Console.WriteLine("coin is head");
-        numOfTimeCoinIsHead = numOfTimeCoinIsHead + 1;
-        Console.WriteLine("numOfTimeCoinIsHead : " + numOfTimeCoinIsHead);
-    }
-    else
-    {
-        Console.WriteLine("coin is tail");
-        numOfTimeCoinIstail = numOfTimeCoinIstail + 1;
-        Console.WriteLine("numOfTimeCoinIstail : " + numOfTimeCoinIstail);
-    }
-
+    Console.WriteLine("number is even");
 }
-        Console.WriteLine(numOfTimeCoinIsHead);
-        Console.WriteLine(numOfTimeCoinIstail);
-    int percentageOfHead = (numOfTimeCoinIsHead *100 / num);
-        Console.WriteLine("percentageOfHead : " + percentageOfHead);
-    int percentageOfTail = (numOfTimeCoinIstail * 100 / num);
-        Console.WriteLine("percentageOfTail : " + percentageOfTail);
-        Console.Read();
-
-
+else
+{
+    Console.WriteLine("number is odd");
+}
