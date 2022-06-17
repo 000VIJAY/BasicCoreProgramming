1bf1b9a87b2c018dfa077e78a92caeb23eacbf41
000VIJAY <vijay893689@gmail.com>
2022-06-17 12:17:54 +0530

    Added how to find remainder and quotient

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
-﻿Console.WriteLine("Write three numbers");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-int num3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("num1 : " + num1);
-Console.WriteLine("num2 : " + num2);
-Console.WriteLine("num3 : " + num3);
-if (num1 > num2 && num1 > num3)
-{
-    Console.WriteLine("Greatest Number : " + num1);
-}
-else if (num2 > num1 && num2 > num3)
-{
-    Console.WriteLine("Greatest number : " + num2);
-}
-else if (num3 > num1 && num3 > num2)
-{
-    Console.WriteLine("Greatest Number : " + num3);
-}
-else
-{
-    Console.WriteLine("All three numbers are equal");
-}
+﻿    Console.WriteLine("Write the dividend number");
+
+int dividendNum = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Write the divisor number");
+int divisorNum = Convert.ToInt32(Console.ReadLine());
+
+int quotient = dividendNum / divisorNum;
+
+    Console.WriteLine("quotient : " + quotient);
+int remainder = dividendNum % divisorNum;
+
+    Console.WriteLine("remainder : " + remainder);
