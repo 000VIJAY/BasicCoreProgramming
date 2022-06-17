5663c4d84e6513a223b9b0d5c7f83559d2c418a9
000VIJAY <vijay893689@gmail.com>
2022-06-17 11:59:59 +0530

    Added Swapping Of Two Number

--- Program.cs
+++ Program.cs
@@ -1,36 +1,11 @@
-﻿Console.WriteLine("Welcome to flip coin");
-Console.WriteLine("Write Number of time wants to flip coin ");
-int head = 0;
-int numOfTimeCoinIstail = 0;
-int numOfTimeCoinIsHead = 0;
-int num = Convert.ToInt32(Console.ReadLine());
-int[] coin = new int[num];
-for (int i = 0; i < coin.Length; i++)
-{
-    Random rnd = new Random();
-    int flipCoin = rnd.Next(2);
-    Console.WriteLine("flipCoin : " + flipCoin);
-    
-    if (flipCoin <= head)
-    {
-        Console.WriteLine("coin is head");
-        numOfTimeCoinIsHead = numOfTimeCoinIsHead + 1;
-        Console.WriteLine("numOfTimeCoinIsHead : " + numOfTimeCoinIsHead);
-    }
-    else
-    {
-        Console.WriteLine("coin is tail");
-        numOfTimeCoinIstail = numOfTimeCoinIstail + 1;
-        Console.WriteLine("numOfTimeCoinIstail : " + numOfTimeCoinIstail);
-    }
+﻿Console.WriteLine("Write two number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-}
-        Console.WriteLine(numOfTimeCoinIsHead);
-        Console.WriteLine(numOfTimeCoinIstail);
-    int percentageOfHead = (numOfTimeCoinIsHead *100 / num);
-        Console.WriteLine("percentageOfHead : " + percentageOfHead);
-    int percentageOfTail = (numOfTimeCoinIstail * 100 / num);
-        Console.WriteLine("percentageOfTail : " + percentageOfTail);
-        Console.Read();
-
-
+Console.WriteLine("num1 : " + num1);
+Console.WriteLine("num2 : " + num2);
+int num3 = num1;
+    num1 = num2;
+    num2 = num3;
+Console.WriteLine("after swapping num1 : " + num1);
+Console.WriteLine("after swapping num2 : " + num2);
