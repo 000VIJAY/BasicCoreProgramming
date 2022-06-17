5c8e100b91f58d9a2d8831496a49e29051e94eb1
000VIJAY <vijay893689@gmail.com>
2022-06-17 12:04:13 +0530

    Added Character is vowel or consonant

--- Program.cs
+++ Program.cs
@@ -1,36 +1,25 @@
-﻿Console.WriteLine("Welcome to flip coin");
-Console.WriteLine("Write Number of time wants to flip coin ");
-int head = 0;
-int numOfTimeCoinIstail = 0;
-int numOfTimeCoinIsHead = 0;
-int num = Convert.ToInt32(Console.ReadLine());
-int[] coin = new int[num];
-for (int i = 0; i < coin.Length; i++)
+﻿Console.WriteLine("write the character");
+char word = Convert.ToChar(Console.ReadLine());
+
+
+switch (word)
 {
-    Random rnd = new Random();
-    int flipCoin = rnd.Next(2);
-    Console.WriteLine("flipCoin : " + flipCoin);
-    
-    if (flipCoin <= head)
-    {
-        Console.WriteLine("coin is head");
-        numOfTimeCoinIsHead = numOfTimeCoinIsHead + 1;
-        Console.WriteLine("numOfTimeCoinIsHead : " + numOfTimeCoinIsHead);
-    }
-    else
-    {
-        Console.WriteLine("coin is tail");
-        numOfTimeCoinIstail = numOfTimeCoinIstail + 1;
-        Console.WriteLine("numOfTimeCoinIstail : " + numOfTimeCoinIstail);
-    }
-
+    case 'a':
+        Console.WriteLine("The alphabet is vowel");
+        break;
+    case 'e':
+        Console.WriteLine("The alphabet is vowel");
+        break;
+    case 'i':
+        Console.WriteLine("The alphabet is vowel");
+        break;
+    case 'o':
+        Console.WriteLine("The alphabet is vowel");
+        break;
+    case 'u':
+        Console.WriteLine("The alphabet is vowel");
+        break;
+    default:
+        Console.WriteLine("The alphabet is consonant");
+        break;
 }
-        Console.WriteLine(numOfTimeCoinIsHead);
-        Console.WriteLine(numOfTimeCoinIstail);
-    int percentageOfHead = (numOfTimeCoinIsHead *100 / num);
-        Console.WriteLine("percentageOfHead : " + percentageOfHead);
-    int percentageOfTail = (numOfTimeCoinIstail * 100 / num);
-        Console.WriteLine("percentageOfTail : " + percentageOfTail);
-        Console.Read();
-
-
